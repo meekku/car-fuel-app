efeeecc881f8c081cac9abd524eaefbfcf4706d7
Melina Kamunen <melina.kamunen@edu.turkuamk.fi>
2022-04-24 14:06:41 +0300

    First doings to start with the app

---
+++ src/components/AllCarExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const AllCarExpenses = () => {
+    return (
+        <div className ="inc-exp-container">
+            <div>
+                <h4>Total refueling expenses over all cars</h4>
+                <p className="money minus">-$0.00</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/AllCars.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const AllCars = () => {
+    return (
+        <>
+            <h4>Total refueling expenses over all cars</h4>
+            <h1>$0.00</h1>
+        </>
+    )
+}
